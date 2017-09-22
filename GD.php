84d0b853bbf60fec186c44a7e881aa0c7231cec8
gosha22008 <gosha22008@yandex.ru>
2017-09-22 20:51:35 +0300

    переделано

--- GD.php
+++ GD.php
@@ -1,17 +1,25 @@
 <?php
-function generateSertif($i,$name){
-$image = imagecreatetruecolor(300,300);
-$whiteColor = imagecolorallocate($image,255,255,255);
-$textColor = imagecolorallocate($image,000,000,000);
-$fontFile ='a_AlternaSw.TTF';
-imagefill($image,0,0,$whiteColor);
-imagettftext($image,45,10,20,200,$textColor,$fontFile,'Сертификат');
-imagettftext($image,45,30,40,200,$textColor,$fontFile,'оценка');
-imagettftext($image,45,60,70,-200,$textColor,$fontFile,$i);
-imagettftext($image,45,10,20,200,$textColor,$fontFile,$name);
-   header('Content-Type: image/png');
-imagepng($image);
-imagedestroy($image);
+function generateSertif($i, $name, $numb)
+{
+    $image = imagecreatetruecolor(500, 350);
+    $whiteColor = imagecolorallocate($image, 255, 255, 255);
+    $textColor = imagecolorallocate($image, 000, 000, 000);
+    $fontFile = 'a.ttf';
+    imagefill($image, 0, 0, $whiteColor);
+    imagettftext($image, 45, 0, 0, 50, $textColor, $fontFile, 'Сертификат :');
+    imagettftext($image, 45, 0, 0, 200, $textColor, $fontFile, 'оценка:');
+    imagettftext($image, 45, 0, 200, 200, $textColor, $fontFile, $numb);
+    imagettftext($image, 25, 0, 0, 250, $textColor, $fontFile, $i);
+    imagettftext($image, 25, 0, 0, 100, $textColor, $fontFile, $name);
+    header('Content-Type: image/png');
+    imagepng($image);
+    imagedestroy($image);
 }
 
+$i = file_get_contents('numb.txt');
+$numb = $i + 2;
+$name = file_get_contents('name.txt');
+$name = "на имя $name";
+$i = "количество верных ответов = $i";
+generateSertif($i, $name, $numb);
 ?>
--- test.php
+++ test.php
@@ -1,68 +1,71 @@
 <?php
 ob_start();
 ?>
-<!DOCTYPE>
-<html lang="ru">
-<head>
-    <title>Задание7</title>
-    <meta charset="utf-8">
-</head>
+    <!DOCTYPE>
+    <html lang="ru">
+    <head>
+        <title>Задание7</title>
+        <meta charset="utf-8">
+    </head>
+    <?php
+    $aa = $_GET;
+    $k = key($aa);
+    if (file_exists(__DIR__ . '/DownloadedTests/' . $_GET[$k])) {
+        $file = file_get_contents(__DIR__ . '/DownloadedTests/' . $_GET[$k]);
+        $file1 = json_decode($file, true);
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        //http_response_code(404);
+        die('такого файла не найдено!');
+
+    }
+    ?>
+    <body>
+    <h3>
+        <a href="admin.php" style="color: black">Загрузить тест</a><br>
+        <a href="list.php" style="color: black">К списку тестов</a>
+    </h3>
+    <form action="" method="POST">
+        <?php foreach ($file1 as $k => $v) { ?>
+            <fieldset>
+                <legend><?= "$k." . $file1[$k]["q$k"] ?></legend>
+                <label><input name="q<?= $k ?>" value="0" type="radio"> <?= $file1[$k]['answer']['a0'] ?></label>
+                <label><input name="q<?= $k ?>" value="1" type="radio"> <?= $file1[$k]['answer']['a1'] ?></label>
+                <label><input name="q<?= $k ?>" value="2" type="radio"> <?= $file1[$k]['answer']['a2'] ?></label>
+            </fieldset>
+        <?php } ?>
+        <input value="Отправить" type="submit">
+    </form>
+    </body>
+    </html>
 <?php
-$aa = $_GET;
-$k = key($aa);
-if (file_exists(__DIR__.'/DownloadedTests/' . $_GET[$k])){
-    $file = file_get_contents(__DIR__.'/DownloadedTests/' . $_GET[$k]);
-    $file1 = json_decode($file, true);
-}else {
-   header("HTTP/1.0 404 Not Found");
-   //http_response_code(404);
-   die('такого файла не найдено!') ;
-   ob_end_flush();
-}
-?>
-<body>
-<h3>
-    <a href="admin.php" style="color: black">Загрузить тест</a><br>
-    <a href="list.php" style="color: black">К списку тестов</a>
-</h3>
-<form action="" method="POST">
-    <?php foreach ($file1 as $k => $v) { ?>
-        <fieldset>
-            <legend><?= "$k." . $file1[$k]["q$k"] ?></legend>
-            <label><input name="q<?= $k ?>" value="0" type="radio"> <?= $file1[$k]['answer']['a0'] ?></label>
-            <label><input name="q<?= $k ?>" value="1" type="radio"> <?= $file1[$k]['answer']['a1'] ?></label>
-            <label><input name="q<?= $k ?>" value="2" type="radio"> <?= $file1[$k]['answer']['a2'] ?></label>
-        </fieldset>
-    <?php } ?>
-    <input value="Отправить" type="submit">
-</form>
-</body>
-</html>
-<?php
-include_once 'GD.php';
+//include_once 'GD.php';
 $priem = $_POST;
 $i = 0;
 if (!empty($priem)) {
     foreach ($file1 as $key => $value) {
-            if ($file1[$key]['correct'] == $priem["q$key"]) {
-                $i++;
-                echo "$key -- Верно<br>";
-            } else echo "$key -- Неверно<br>";
-        }
-        echo $i;
+        if ($file1[$key]['correct'] == $priem["q$key"]) {
+            $i++;
+            echo "$key -- Верно<br>";
+            file_put_contents('numb.txt',$i);
+        } else echo "$key -- Неверно<br>";
+    }
+    echo $i;
+    //file_put_contents('numb.txt',$i);
     echo '
 <form method="post">
 <input type="text" name="name">введите имя
 <input type="submit" value="отправить">
 </form>
 ';
-    if (isset($_POST['name']) and !empty($_POST['name'])){
+    if (isset($_POST['name']) and !empty($_POST['name'])) {
         $name = $_POST['name'];
-        echo $name;
-        //print_r(gd_info());
-       generateSertif($i,$name);
+
+        file_put_contents('name.txt', $name);
+        header('Location: GD.php ');
+
     }
 }
 
-
+ob_end_flush();
 ?>
